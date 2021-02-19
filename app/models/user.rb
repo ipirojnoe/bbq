76c96fd1fedad8194b6b2a2754e4eb8014c0996b
Ilya <deminiiii@gmail.com>
2021-02-19 07:35:26 +0300

    Install devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,18 @@
 class User < ApplicationRecord
-  EMAIL_REGEX = /\A[a-z\d_+.\-]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
 
   has_many :events
 
   validates :username, presence: true, length: {maximum: 35}
-  validates :email, presence: true, length: {maximum: 255}, uniqueness: true, format: { with: EMAIL_REGEX }
+
+  before_validation :set_name, on: :create
+
+  private
+
+  def set_name
+    self.username = "Товарисч №#{rand(777)}" if self.username.blank?
+  end
 end
