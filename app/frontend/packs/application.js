5c5dbd945cda1243872f8b5205c08465f531978e
Ilya <deminiiii@gmail.com>
2021-02-23 21:14:57 +0300

    Add datetimepicker

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -3,6 +3,9 @@ require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
 import 'bootstrap/dist/js/bootstrap'
+import "air-datepicker/dist/js/datepicker.min"
+
+import '../scripts/events'
 
 // stylesheets
 import '../styles/application'
---
+++ app/frontend/scripts/events.js
@@ -0,0 +1,5 @@
+$(function () {
+  $('#datetimepicker').datepicker({
+    minDate: new Date()
+  })
+});
