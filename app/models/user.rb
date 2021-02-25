6cc4dfa219146743da37928710f3f9d7f62c3383
Ilya <deminiiii@gmail.com>
2021-02-25 09:26:21 +0300

    Link subscriptions to user with subscribed email

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,9 +12,16 @@ class User < ApplicationRecord
 
   before_validation :set_name, on: :create
 
+  after_commit :link_subscriptions, on: :create
+
   private
 
   def set_name
     self.username = "Товарисч №#{rand(777)}" if self.username.blank?
   end
+
+  def link_subscriptions
+    Subscription.where(user_id: nil, user_email: self.email)
+      .update_all(user_id: self.id)
+  end 
 end
