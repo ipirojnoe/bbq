fe41c60a940918fec198b9789b4af334c8161858
Ilya <deminiiii@gmail.com>
2021-02-28 12:36:12 +0300

    Display event photos

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,6 +9,34 @@ module ApplicationHelper
     end
   end
 
+  def user_avatar_thumb(user)
+    if user.avatar.file.present?
+      user.avatar.thumb.url
+    else
+      asset_pack_path('media/images/default.png')
+    end
+  end
+
+  def event_photo(event)
+    photos = event.photos.persisted
+
+    if photos.any?
+      photos.sample.photo.url
+    else
+      asset_pack_path('media/images/event.jpg')
+    end
+  end
+
+  def event_thumb(event)
+    photos = event.photos.persisted
+
+    if photos.any?
+      photos.sample.photo.thumb.url
+    else
+      asset_pack_path('media/images/event_thumb.jpg')
+    end
+  end
+
   def flash_class(name)
     case name
     when 'notice' then 'info'
