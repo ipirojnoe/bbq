560bfb00375054819be7f7a8fd1af76f18765922
Ilya <deminiiii@gmail.com>
2021-03-01 07:44:45 +0300

    Send mail notifications of new subscriber and new comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,6 +7,7 @@ class CommentsController < ApplicationController
     @new_comment.user = current_user
 
     if @new_comment.save
+      notify_subscribers(@event, @new_comment)
       redirect_to @event, success: I18n.t('controllers.comments.created')
     else
       render 'events/show', danger: I18n.t('controllers.comments.error')
@@ -26,6 +27,15 @@ class CommentsController < ApplicationController
   end
 
   private
+
+  def notify_subscribers(event, comment)
+    all_emails = (event.subscriptions.map(&:user_email) + [event.user.email]).uniq
+
+    all_emails.each do |mail|
+      EventMailer.comment(event, comment, mail).deliver_now
+    end
+  end
+
     def set_event
       @event = Event.find(params[:event_id])
     end
