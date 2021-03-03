b7b0fb992f2991511915fd4d5e1f07f829874e7a
Ilya <deminiiii@gmail.com>
2021-03-03 15:43:52 +0300

    Show map on event page

--- app/frontend/scripts/events.js
+++ app/frontend/scripts/events.js
@@ -3,3 +3,40 @@ $(function () {
     minDate: new Date()
   })
 });
+
+ymaps.ready(init);
+var myMap;
+
+function init(){
+  map = document.getElementById('map')
+
+  if(map){
+    address = map.getAttribute('data-address');
+  
+    myMap = new ymaps.Map("map", {
+        center: [55.76, 37.64],
+        zoom: 10
+    });
+  
+    myGeocoder = ymaps.geocode(address);
+  
+    myGeocoder.then(
+      function (res) {
+        coordinates = res.geoObjects.get(0).geometry.getCoordinates();
+  
+        myMap.geoObjects.add(
+            new ymaps.Placemark(
+              coordinates,
+              {iconContent: address},
+              {preset: 'islands#blueStretchyIcon'}
+            )
+        );
+  
+        myMap.setCenter(coordinates);
+        myMap.setZoom(15);
+      }, function (err) {
+        alert('Что-то пошло не так!');
+      }
+    );
+  }
+}
