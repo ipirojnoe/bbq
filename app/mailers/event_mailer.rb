bea302ae8f49c37f02890736503fcd1515c23c06
Ilya <deminiiii@gmail.com>
2021-03-05 04:36:01 +0300

    I18n mail subjects the right way

--- app/mailers/event_mailer.rb
+++ app/mailers/event_mailer.rb
@@ -4,20 +4,20 @@ class EventMailer < ApplicationMailer
     @name = subscription.user_name
     @event = event
 
-    mail to: event.user.email, subject: "Новая подписка на \"#{event.title}\""
+    mail to: event.user.email, subject: default_i18n_subject(title: event.title)
   end
 
   def comment(event, comment, email)
     @comment = comment
     @event = event
 
-    mail to: email, subject: "Новый комментарий @ #{event.title}"
+    mail to: email, subject: default_i18n_subject(title: event.title)
   end
 
   def photo(event, photo, email)
     @photo = photo
     @event = event
     
-    mail to: email, subject: "Новая фотография у события \"#{event.title}\""
+    mail to: email, subject: default_i18n_subject(title: event.title)
   end
 end
