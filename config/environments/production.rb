68505b4f14e8536e6083b4075dd1e4dc4ea6c07c
Ilya <deminiiii@gmail.com>
2021-03-21 22:25:11 +0300

    Setup for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -111,7 +111,7 @@ Rails.application.configure do
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
   
-  config.action_mailer.default_url_options = { :host => 'meetbbq.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => ENV['HOST'] }
 
   config.action_mailer.delivery_method = :mailjet
 end
