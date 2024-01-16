c64af65f875410c37e792c301e281b7dd4879b15
Ilya <deminiiii@gmail.com>
2024-01-16 12:33:04 +0300

    Add ssh private key for capistrano

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,6 +6,9 @@
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
+set :ssh_options, {
+  keys: "/servers/bbq_ssh_private_key"
+}
 
 server "bbq.ipirojnoe.ru", user: "client", roles: %w[app db web]
 
