91c5223ccdaad9eedd73fbee49fa973b6617f37e
Ilya <deminiiii@gmail.com>
2024-01-16 15:12:31 +0300

    Set git branch for capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,4 +8,4 @@ set :deploy_to, "/home/client/bbq"
 append :linked_files, 'config/database.yml', '.env'
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads'
 
-set :branch, ENV['BRANCH'] if ENV['BRANCH']
+set :branch, 'main'
